ecd2de88471229adf8ddc23640489c330ab47c1f
HMEI83 <101045819+HMEI83@users.noreply.github.com>
2022-10-17 17:09:37 +1100

    init push
    init push

---
+++ emucheck-app/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+  <h2>test</h2>
+  <ul>
+    <li></li>
+  </ul>
+</section>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      test:[
+        {
+          id:"qwe",
+          name:"qwerty"
+        }
+      ],
+      test2:[
+      {
+          id:"qweasd",
+          name:"qwertzxcy"
+        }
+      ]
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ emucheck-app/src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+    <li>
+        <h2>{{test.id}}</h2>
+        <button>Show</button>
+    </li>
+    <li>
+        <h2>{{test.id}}</h2>
+        <button>Show</button>
+    </li>
+</section>
+</template>
+
+<script>
+export default {
+   data() {
+    return{
+        
+    }
+   }
+};
+</script>
