913144dd926b8ac0b5a298a6e645ca562ced4b84
HMEI83 <101045819+HMEI83@users.noreply.github.com>
2022-10-21 16:05:05 +1100

    change header
    change header

--- emucheck-app/src/views/HeaderView.vue
+++ emucheck-app/src/views/HeaderView.vue
@@ -1,18 +1,37 @@
 <template>
   <div>
-  <b-nav pills align="center">
-    <b-nav-item active>Active</b-nav-item>
-    <b-nav-item>Link</b-nav-item>
-    <b-nav-item-dropdown
-      id="my-nav-dropdown"
-      text="Dropdown Menu"
-      toggle-class="nav-link-custom"
-      right
-    >
-      <b-dropdown-item>One</b-dropdown-item>
-      <b-dropdown-item>Two</b-dropdown-item>
-      <b-dropdown-item>Three</b-dropdown-item>
-    </b-nav-item-dropdown>
-  </b-nav>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-5">
+        <b-nav-item to="/Login">Link</b-nav-item>
+        <b-nav-item href="#">Link2</b-nav-item>
+        <b-nav-item href="#">Link3</b-nav-item>
+        <b-nav-item href="#">Link4</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <!-- <b-nav-form >
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form> -->
+
+
+        <b-navbar-nav>
+        <b-nav-item href="#">Login</b-nav-item>
+        <b-nav-item href="#">Sign Up</b-nav-item>
+      </b-navbar-nav>
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">中文</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </div>
 </template>
